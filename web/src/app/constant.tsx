491b0b780d1704221c0072b7ac246c5fa5514ad3
lai hung wei <128201642+s990093@users.noreply.github.com>
2024-08-18 00:57:35 +0800

    new commit

--- web/src/app/constant.tsx
+++ web/src/app/constant.tsx
@@ -4,10 +4,10 @@ require("dotenv").config();
 // 從環境變量中讀取
 // 從環境變量中讀取，如果不存在則使用默認值
 // 從環境變量中讀取，如果不存在則使用默認值
-export const DJANGO_IP = process.env.NEXT_PUBLIC_DJANGO_IP || "192.168.0.135";
-export const DJANGO_PORT = process.env.NEXT_PUBLIC_DJANGO_PORT || "8000";
-export const NGINX_PORT = process.env.NEXT_PUBLIC_NGINX_PORT || "8001";
-export const NGINX_T_PORT = process.env.NEXT_PUBLIC_NGINX_T_PORT || "8002";
+export const DJANGO_IP = process.env.NEXT_PUBLIC_DJANGO_IP;
+export const DJANGO_PORT = process.env.NEXT_PUBLIC_DJANGO_PORT;
+export const NGINX_PORT = process.env.NEXT_PUBLIC_NGINX_PORT;
+export const NGINX_T_PORT = process.env.NEXT_PUBLIC_NGINX_T_PORT;
 
 export const DJANGO_URL = (url: string): string => {
   return `http://${DJANGO_IP}:${DJANGO_PORT}/${url}`;
--- web/src/app/swagger/page.tsx
+++ web/src/app/swagger/page.tsx
@@ -1,5 +1,6 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css"; // Import the default swagger-ui styles
+import { DJANGO_IP } from "../constant";
 
 require("dotenv").config();
 // const { SwaggerTheme, SwaggerThemeNameEnum } = require("swagger-themes");
@@ -10,7 +11,7 @@ require("dotenv").config();
 const SwaggerPage = () => {
   return (
     <div style={{ height: "100vh" }}>
-      <SwaggerUI url="http://localhost:8000/swagger/?format=openapi" />
+      <SwaggerUI url={`http://${DJANGO_IP}:8000/swagger/?format=openapi`} />
     </div>
   );
 };
